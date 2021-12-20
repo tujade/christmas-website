043cd1d84fb2ec0845b755b0975cb4ad4f8901f6
tujade <jade.mundim@gmail.com>
2021-12-20 19:55:11 -0300

    Scroll Reveal  HTML and JS  :bookmark:

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,3 +116,16 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+  })
+  
+  sr.reveal(`.home__img, .new__container, .footer__container`)
+  sr.reveal(`.home__data`, {delay: 500})
+  sr.reveal(`.giving__content, .gift__card`,{interval: 100})
+  sr.reveal(`.celebrate__data, .message__form, .footer__img1`,{origin: 'left'})
+  sr.reveal(`.celebrate__img, .message__img, .footer__img2`,{origin: 'right'})
